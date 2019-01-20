84c87b3f3a5ed14be44fda44db296abf9d2d1db1
ravulaprasad <ravulasaisimhaprasad@gmail.com>
2019-01-20 12:29:10 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,10 @@
+username="David"
+password='%ecvx8&sf*'
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+class sample:
+    pass
+class login:
+    pass
---
+++ b.py
@@ -0,0 +1,8 @@
+import a
+print(a.username,a.password)
+k=a.add(10,20)
+print(k)
+m=a.sub(200,190)
+print(m)
+s=a.sample()
+l=a.login()
